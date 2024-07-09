709ee4adf286cf46c14613aee4b465dc9b9af75d
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-10 01:40:28 +0400

    Programmatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import React from 'react';
+import {useRouter} from "next/navigation";
 
 const NewUserPage = () => {
+    const router =  useRouter();
+
     return (
-        <div>
-            NewUserPage
-        </div>
+        <button
+            className='btn btn-primary'
+            onClick={() => router.push('/users')}>
+            Create
+        </button>
     );
 };
 
