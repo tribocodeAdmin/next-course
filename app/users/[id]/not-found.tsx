d90b1ccdcf671243621bf835fbf1bb355cf9e9a0
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-10 02:00:26 +0400

    Handling Not Found Errors

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserNotFoundPage = () => {
+    return (
+        <div>
+            This user does not exist!
+        </div>
+    );
+};
+
+export default UserNotFoundPage;
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {notFound} from "next/navigation";
 
 interface Props {
     params: { id: number }
 }
 const UserDetailPage = ( { params: { id } }: Props) => {
+    if (id > 10) notFound();
+    
     return (
         <div>
             UserDetailPage {id}
