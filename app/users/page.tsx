dd1a5835d79ee6dc164231c638cba513f64db37a
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-09 16:57:49 +0400

    Routing_Overview

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
+import UserTable from "@/app/users/UserTable";
 
-interface User {
-    id: number,
-    name: string
-}
+
 const UserPage = async () => {
-    const res = await  fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-cache' })
-    const users: User[] = await res.json();
+
 
     return (
         <>
             <h1>Users</h1>
-            <p>{new Date().toLocaleTimeString()}</p>
-            <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
-            </ul>
+            <UserTable />
         </>
     );
 };
