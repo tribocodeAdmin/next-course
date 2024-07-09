bd0e151d537f1ea6e5511992e59bf5cb3087b2e0
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-09 15:02:29 +0400

    Client and Server Components

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import AddToCart from "@/app/components/AddToCart";
+
+const ProductCard = () => {
+    return (
+        <div>
+            <AddToCart />
+        </div>
+    );
+};
+
+export default ProductCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "@/app/components/ProductCard";
 
 export default function Home() {
   return (
       <main>
         <h1>Home</h1>
           <Link href="/users">Users</Link>
+          <ProductCard />
       </main>
   );
 }
