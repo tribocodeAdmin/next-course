5560592867ac74e4ac6a778cc16453c89dd4b022
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-09 15:26:23 +0400

    Data Fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const UserPage = () => {
+interface User {
+    id: number,
+    name: string
+}
+const UserPage = async () => {
+    const res = await  fetch('https://jsonplaceholder.typicode.com/users')
+    const users: User[] = await res.json();
+
     return (
-        <div>
-            User Page
-        </div>
+        <>
+            <h1>Users</h1>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
     );
 };
 
