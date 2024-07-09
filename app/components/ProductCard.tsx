b8deb4a4117a5ef00e81ba2f10628d18b984c551
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-09 16:40:51 +0400

    Styling

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 
 import React from 'react';
 import AddToCart from "@/app/components/AddToCart";
+import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
     return (
-        <div>
+        <div className={styles.card}>
             <AddToCart />
         </div>
     );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,12 +5,13 @@ interface User {
     name: string
 }
 const UserPage = async () => {
-    const res = await  fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } })
+    const res = await  fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-cache' })
     const users: User[] = await res.json();
 
     return (
         <>
             <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
             <ul>
                 {users.map(user => <li key={user.id}>{user.name}</li>)}
             </ul>
