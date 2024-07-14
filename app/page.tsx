816802c7b2ad4281a12e26df35429faaa7f30d82
Manolo Ramon Manatlao <tribo.code@outlook.com>
2024-07-14 08:17:15 +0400

    Optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,46 @@
+
 import Link from "next/link";
 import ProductCard from "@/app/components/ProductCard";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import Image from "next/image";
+import coffee from '@/public/images/espreso.jpg'
+import {Metadata} from "next";
+import HeavyComponent from "@/app/components/HeavyComponent";
+import {useState} from "react";
 
 export default async function Home() {
     const session = await getServerSession(authOptions)
 
+
   return (
-      <main>
+      <main className="relative h-screen">
         <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+
           <Link href="/users">Users</Link>
           <ProductCard />
+
+          {/*Optimization*/}
+          <Image
+              src="https://bit.ly/react-cover"
+              alt='coffee'
+              fill
+              className="object-cover"
+              sizes="(max-width: 480px) 100vw, (max-witdth: 768px) 50vw, 33vw"
+              quality={100}
+              priority
+          />
       </main>
   );
 }
+
+// export async function generateMetadata(): Promise<Metadata> {
+//     const product = await fetch('')
+//
+//     return {
+//         title: 'product.title',
+//         description: '...'
+//     }
+// }
 
 // TODO: 0068_3_Creating_an_Email_Template
